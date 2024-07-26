6f5b17860d14674974135fce20024aee124fb63b
Franklin <franklinmachado1985@gmail.com>
2024-07-26 09:49:30 +0200

    adicionando modulo 09

---
+++ mod-09/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('header button').click(function () {
+        //alert("Expandir formulario")
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function () {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        console.log(enderecoDaNovaImagem);
+        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+
+        $(`
+            <div class= "overlay-imagem-link">
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanmho real">
+                Ver imagem em tamanho real
+            </a>
+            </div>
+        
+        
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+    })
+})
