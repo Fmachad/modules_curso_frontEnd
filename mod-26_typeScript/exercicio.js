e1c2db6b6f05410bc266a677828c0d0ccfa853af
Franklin <franklinmachado1985@gmail.com>
2024-08-09 08:14:48 +0200

    upload_module

--- mod-26_typeScript/exercicio.js
+++ mod-26_typeScript/exercicio.js
@@ -6,6 +6,6 @@ function saudacao(nome) {
     return "Olá " + nome;
 }
 let resultado = multiplicar(7, 3);
-console.log(resultado); // Output: 12
+console.log(resultado);
 let cumprimento = saudacao("Franklin");
 console.log(cumprimento);
